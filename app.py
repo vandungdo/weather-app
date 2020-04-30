58de21cba16cb2b887ae7dfa67043c3c4e15baf7
VanDung <dung.dovan@campus.tu-berlin.de>
2020-04-30 17:21:34 +0200

    change unit of temperature

--- app.py
+++ app.py
@@ -7,20 +7,21 @@ app = Flask(__name__)
 @app.route('/',methods =['GET','POST'])
 def home():
     response = []
+    weather = {}
     if request.method=='POST':
         city = request.form.get('city')
         response.append(requests.get('http://api.openweathermap.org/data/2.5/weather?q='+city+'&appid='+apiKey()).json())
-    weather = {
-        'City': response[0]['name'],
-        'Location':response[0]['coord'],
-        'Main_weather' : response[0]['weather'][0]['main'],
-        'Description_weather' : response[0]['weather'][0]['description'],
-        'Temperatur': response[0]['main']['temp'],
-        'Feels_like': response[0]['main']['feels_like'],
-        'Temp_min': response[0]['main']['temp_min'],
-        'Temp_max': response[0]['main']['temp_max'],
-        'Humidity': response[0]['main']['humidity']
-    }
+        weather = {
+            'City': response[0]['name'],
+            'Location':response[0]['coord'],
+            'Main_weather' : response[0]['weather'][0]['main'],
+            'Description_weather' : response[0]['weather'][0]['description'],
+            'Temperatur': round(response[0]['main']['temp']-273.15),
+            'Feels_like': round(response[0]['main']['feels_like']-273.15),
+            'Temp_min': round(response[0]['main']['temp_min']-273.15),
+            'Temp_max': round(response[0]['main']['temp_max']-273.15),
+            'Humidity': response[0]['main']['humidity']
+        }
     return render_template('index.html',weather=weather)
 
 # @app.route('/city',methods=['GET','POST'])
